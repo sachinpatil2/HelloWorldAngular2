eb67ebf676969309288ee80cedc3d32c2db12925
Sachin Patil <sachin.patil5@ibm.com>
2019-11-27 15:03:29 +0530

    Revert "added custom directive and nested components."
    This reverts commit 81aa737848608d8ead194b7104f52be50b0c5cae.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/layout/header.component';
-import { BoldHoverDirective } from './directive/bold-hover.directive';
 
 @NgModule({
   imports: [ BrowserModule ],
   providers: [],
-  declarations: [
-                  AppComponent ,
-                  HeaderComponent,
-                  BoldHoverDirective
-                ],
+  declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
